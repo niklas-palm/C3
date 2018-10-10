215ff13e1698af732c613f535cb494d908eda6e6
Ubuntu <niklas-palm@hotmil.com>
2018-10-10 12:34:03 +0000

    intermediate push

--- tasks.py
+++ tasks.py
@@ -1,6 +1,18 @@
 from celery import Celery
 
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
+app = Celery('tasks', backend='rpc://', broker='pyamqp://guest@localhost//')
+app.config_from_object('celeryconfig')
+
+#Give a certain task low priority
+#task_routes = {
+#    'tasks.add': 'low-priority',
+#}
+
+#A certain task can only be processed 10 times per minute
+#task_annotations = {
+#    'tasks.add': {'rate_limit': '10/m'}
+#}
+
 
 @app.task
 def add(x, y):
